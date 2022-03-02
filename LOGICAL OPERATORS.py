8fe2dd9e076ff8a3ca14baa1f4fdba6fe7bad9ea
Elton566 <99070242+Elton566@users.noreply.github.com>
2022-03-03 02:11:43 +0530

    project 1

---
+++ LOGICAL OPERATORS.py
@@ -0,0 +1,13 @@
+#LOGICAL OPERATORS
+a=5
+b=10
+print(a==5 and b==10)
+
+print(a==10 and b==5)
+
+print(a==5 or b==6)
+
+print(a==6 or b==7)
+
+print(not a==5)
+print(not a==8)
---
+++ arithmetic operators.py
@@ -0,0 +1,72 @@
+#arithmetic operators
+x= 10
+y= 5
+
+print(x+y)
+
+print(x-y)
+
+print(x*y)
+
+print(x**y)
+
+print(x/y)
+
+print(x//y)
+
+print(x%y)
+
+#assignment operators
+a=5
+print(a)
+
+a=5
+a+=3
+print(a)
+
+a=5
+a-=7
+print(a)
+
+a=10
+a/=5
+print(a)
+
+a =10
+a*=5
+print(a)
+
+#comparison operators
+a=10
+print(a<5)
+
+print(a>5)
+
+print(a<=5)
+
+print(a>=5)
+
+print(a==5)
+
+#logical operators
+a=5
+b=10
+print(a==5 and b==10)
+
+print(a==10 and b==5)
+
+print(a==5 or b==6)
+
+print(a==6 or b==7)
+
+print(not a==5)
+print(not a==8)
+
+#membership operators
+x=(1,2,3)
+print(1 in x)
+print(10 in x)
+
+print(3 not in x)
+print(5 not in x)
+
